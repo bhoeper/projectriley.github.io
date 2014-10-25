bd17d36f6aec29f4e53437e602a9a8133e4070cd
Bryce Hoeper <Brycehoeper@gmail.com>
2014-10-25 09:44:57 -0700

    Initial push
    first big push!

---
+++ search/tipuesearch_content.js
@@ -0,0 +1,15 @@
+var tipuesearch = {"pages": [
+     {"title": "Search results for Education Discount", "text": "Wiki search results for education discount", "tags": "education discount wiki .edu", "loc": "https://jira.nyc.squarespace.net/wiki/dosearchsite.action?queryString=education%20discount&startIndex=0&where=conf_all"},
+     {"title": "Search results for Creating a Jira", "text": "Wiki search for how to create a Jira", "tags": "Jira how to create a jira", "loc": "https://jira.nyc.squarespace.net/wiki/dosearchsite.action?queryString=how%20to%20create%20a%20jira&startIndex=0&where=conf_all"},
+     {"title": "What to do if you forgot to clock in or out", "text": "Forgot to clock in or out?  That’s ok, here is what to do", "tags": "time clock forgot to clock in out shiftplanning", "loc": "https://customercare.squarespace.com/scheduling/"},
+     {"title": "Site Explorer", "text": "Site Explorer", "tags": "site exporer", "loc": "https://site-explorer.intranet.squarespace.net/"},
+     {"title": "Billing Escalations Procedures", "text": "Billing Escalations Procedures.", "tags": "Billing Escalations Procedures", "loc": "https://jira.nyc.squarespace.net/wiki/display/sup/Billing+Escalation+Procedures"},    
+     {"title": "How to add an image block.", "text": "How to add an image block to your site", "tags": "image, block,", "loc": "http://help.squarespace.com/guides/about-the-image-block"},
+     {"title": "Tipr, a small and simple jQuery tooltip plugin", "text": "Tipr is a small and simple jQuery tooltip plugin. It's free and open source. Tipr displays attractive tooltips, and it's a shade under 3 KB, CSS included.", "tags": "JavaScript", "loc": "http://www.tipue.com/tipr"},
+     {"title": "The Tipue blog", "text": "An occasional blog covering CSS, jQuery and web development.", "tags": "", "loc": "http://www.tipue.com/blog"},
+     {"title": "Tipue jQuery plugins", "text": "The Tipue jQuery plugins are free, open source and responsive. We offer a range of flexible support plans for our jQuery plugins, including free.", "tags": "", "loc": "http://www.tipue.com/jquery"},
+     {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London. We've been around for over a decade. We design innovative add-ins, plugins, code and features with Perl, MySQL and jQuery.", "tags": "", "loc": "http://www.tipue.com/about"}
+]};
+
+
+		
